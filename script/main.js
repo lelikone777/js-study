44308fd1a77dfebf3f71e641638d6cd2b647bc69
lelikone777 <2257855@users.noreply.github.com>
2020-06-05 19:14:52 +0300

    1

---
+++ script/main.js
@@ -0,0 +1,45 @@
+<<<<<<< Updated upstream
+=======
+let money = 500; 
+let income = 100; 
+let addExpenses = 'Вода, еда, фарфор'; 
+let deposit = true; 
+let mission = 1000; 
+let period = 10;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев'); 
+console.log('Цель заработать ' + mission + ' юани');
+ 
+addExpenses.toLowerCase();
+addExpenses.split(' ');
+console.log(addExpenses.split(' ').toLowerCase());
+
+
+
+
+
+
+
+
+//    - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+//    - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+
+//    - Вывести в консоль budgetDay
+
+
+
+
+
+// 3) Проверить, чтобы все работало и не было ошибок в консоли
+
+
+
+// 4) Добавить папку или ветку со вторым уроком в свой репозиторий на GitHub
+>>>>>>> Stashed changes
