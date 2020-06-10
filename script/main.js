cb5037d638221fcf9ba4a6ac51944c9d966f0b2c
lelikone777 <2257855@users.noreply.github.com>
2020-06-10 21:40:13 +0300

    комит финал 1.0

--- script/main.js
+++ script/main.js
@@ -1,39 +1,38 @@
 'use strict';
 // 1
 const income = 100;
-const addExpenses = 'Вода, Еда, Фарфор, МРАМОР';
-const deposit = true;
 const mission = 100;
 const period = 10;
+console.log(typeof income);
 
 
 // 2
-const money = +prompt('Ваш месячный доход?');
-
+const money = +prompt('Ваш месячный доход?', 90);
+console.log(typeof money);
 // 3
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Вода, Еда, Фарфор, МРАМОР');
 
 // 4
 const deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
+console.log(typeof deposit);
 
 // 5
-const expenses1 = prompt('Введите обязательную статью расходов?');
-const amount1 = +prompt('Во сколько это обойдется?');
-const expenses2 = prompt('Введите обязательную статью расходов?');
-const amount2 = +prompt('Во сколько это обойдется?');
+const expenses1 = prompt('Введите обязательную статью расходов?' , 'Вода');
+const amount1 = +prompt('Во сколько это обойдется?', 10);
+const expenses2 = prompt('Введите обязательную статью расходов?', 'Еда');
+const amount2 = +prompt('Во сколько это обойдется?', 30);
 
 // 6
 const budgetMonth = money - (amount1 + amount2);
-console.log(budgetMonth); 
+console.log('Ваш бюджет на месяц: ' + budgetMonth); 
 
 // 7
 const goal = Math.ceil(mission / budgetMonth);
-console.log(goal);
+console.log('Цель будет достигнута за ' + goal + ' месяцев');
 
 // 8
 const budgetDay = Math.floor(budgetMonth/30);
-console.log(budgetDay);
+console.log('Бюджет на день: ' + budgetDay);
 
 // 9
 if (budgetMonth > 1200) {
