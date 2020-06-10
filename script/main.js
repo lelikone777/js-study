571eb94367ce2892872964886024dbbac97242cd
lelikone777 <2257855@users.noreply.github.com>
2020-06-10 20:13:27 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const money = +prompt('Ваш месячный доход?');
+// console.log(money);
+
+// const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+// console.log(addExpenses);
+
+// const deposit = confirm('Есть ли у вас депозит в банке?');
+// console.log(deposit);
+
+// const expenses1 = prompt('Введите обязательную статью расходов?');
+const amount1 = +prompt('Во сколько это обойдется?');
+// const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount2 = +prompt('Во сколько это обойдется?');
+
+const budgetMonth = money - (amount1 + amount2);
+console.log(budgetMonth); 
+
+const mission = 
