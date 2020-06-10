e24ae21e4b6a5121d931d11df2cd4d0536b905af
lelikone777 <2257855@users.noreply.github.com>
2020-06-10 23:16:05 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,52 +1,55 @@
-'use strict';
+"use strict";
 // 1
 const income = 100;
 const mission = 100;
 const period = 10;
 console.log(typeof income);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' юани');
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " юани");
 
 // 2
-const money = +prompt('Ваш месячный доход?', 90);
-console.log('Ваш месячный доход: ' + money);
+const money = +prompt("Ваш месячный доход?", 90);
+console.log("Ваш месячный доход: " + money);
 console.log(typeof money);
 // 3
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Вода, Еда, Фарфор, МРАМОР');
-console.log('возможные расходы за рассчитываемый период: ' + addExpenses);
+const addExpenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую",
+  "Вода, Еда, Фарфор, МРАМОР"
+);
+console.log("возможные расходы за рассчитываемый период: " + addExpenses);
 console.log(typeof addExpenses);
 console.log(addExpenses.length);
-console.log(addExpenses.toLowerCase().split(' '));
+console.log(addExpenses.toLowerCase().split(" "));
 
 // 4
-const deposit = confirm('Есть ли у вас депозит в банке?');
+const deposit = confirm("Есть ли у вас депозит в банке?");
 console.log(typeof deposit);
 switch (deposit) {
   case true:
-    console.log('депозит в банке присутсвует');
-    break
+    console.log("депозит в банке присутсвует");
+    break;
   case false:
-    console.log('депозита нет');
-    break
+    console.log("депозита нет");
+    break;
 }
 
 // 5
-const expenses1 = prompt('Введите обязательную статью расходов?' , 'Вода');
-const amount1 = +prompt('Во сколько это обойдется?', 10);
-const expenses2 = prompt('Введите обязательную статью расходов?', 'Еда');
-const amount2 = +prompt('Во сколько это обойдется?', 30);
+const expenses1 = prompt("Введите обязательную статью расходов?", "Вода");
+const amount1 = +prompt("Во сколько это обойдется?", 10);
+const expenses2 = prompt("Введите обязательную статью расходов?", "Еда");
+const amount2 = +prompt("Во сколько это обойдется?", 30);
 
 // 6
 const budgetMonth = money - (amount1 + amount2);
-console.log('Ваш бюджет на месяц: ' + budgetMonth); 
+console.log("Ваш бюджет на месяц: " + budgetMonth);
 
 // 7
 const goal = Math.ceil(mission / budgetMonth);
-console.log('Цель будет достигнута за ' + goal + ' месяцев');
+console.log("Цель будет достигнута за " + goal + " месяцев");
 
 // 8
-const budgetDay = Math.floor(budgetMonth/30);
-console.log('Бюджет на день: ' + budgetDay);
+const budgetDay = Math.floor(budgetMonth / 30);
+console.log("Бюджет на день: " + budgetDay);
 
 // 9
 if (budgetMonth > 1200) {
@@ -58,5 +61,3 @@ if (budgetMonth > 1200) {
 } else if (budgetMonth <= 0) {
   console.log("Что то пошло не так");
 }
-
-
