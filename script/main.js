ef72570636ebba17b6542b71c349b650552fee2d
lelikone777 <2257855@users.noreply.github.com>
2020-06-12 14:54:28 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -20,11 +20,14 @@ let getExpensesMonth = function () {
 };
 console.log(getExpensesMonth());
 
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Вода, Еда, Фарфор, МРАМОР');
-console.log('возможные расходы за рассчитываемый период: ' + addExpenses);
+const addExpenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую",
+  "Вода, Еда, Фарфор, МРАМОР"
+);
+console.log("возможные расходы за рассчитываемый период: " + addExpenses);
 console.log(typeof addExpenses);
 console.log(addExpenses.length);
-console.log(addExpenses.toLowerCase().split(' '));
+console.log(addExpenses.toLowerCase().split(" "));
 
 let getAccumulatedMonth = function () {
   return money - getExpensesMonth();
