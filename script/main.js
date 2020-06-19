5b20e5e950ef407f63d2d4c1ae6c6dbe39d3e2eb
lelikone777 <2257855@users.noreply.github.com>
2020-06-19 15:50:55 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,59 +1,96 @@
-'use strict';
+"use strict";
 
-let isNumber = function(n) {
+let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function askNumber() {
+let money;
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min - 1)) + min;
+let start = function () {
+  money = prompt("Ваш месячный доход?");
+
+  while (!isNumber(money)) {
+    money = prompt("Ваш месячный доход?");
+  }
+};
+start();
+
+let appData = {
+  income: {},
+  addIncome: [],
+  expensesMonth: 0,
+  expenses: {}, // основные расходы
+  addExpenses: [], // дополнительные расходы
+  deposit: false,
+  period: 3,
+  budget: money,
+  budgetMonth: 0,
+  budgetDay: 0,
+  mission: function () {
+    prompt("Сколько вы хотите накопить денег?");
+    console.log("Цель накопить " + appData.mission + " рублей");
+  },
+  asking: function () {
+    let addExpenses = prompt("Пересислите возможные расходы через запятую");
+    appData.addExpenses = addExpenses.toLowerCase().split(", ");
+    appData.deposit = confirm("Есть ли у вас депозит в банке?");
+
+    for (let i = 0; i < 2; i++) {
+      let tempText = prompt("Введите обязательную статью расходов");
+
+      let tempMoney;
+
+      while (!isNumber(tempMoney)) {
+        tempMoney = +prompt("Во сколько это обойдется");
+      }
+
+      appData.expenses[tempText] = tempMoney;
     }
-
-    let compNumber = getRandomInt(1, 100);
-    console.log(compNumber); // читерное число компьютера
-
-    function enterNumber() {
-
-        const userNumber = prompt("Угадай число от 1 до 100"); // число пользователя
-
-        if (!userNumber && userNumber !== '0') {
-            return;
-        }
-
-            if (isNumber(userNumber)) {
-                const number = +userNumber;
-
-                if (number > 100 || number < 1) {
-                    alert("Введите число от 1 до 100!");
-
-                } else {
-
-                    if (userNumber < compNumber) {
-
-                        alert('Загаданное число больше');
-
-                    } else if (userNumber > compNumber) {
-
-                        alert('Загаданное число меньше');
-
-                    } else {
-
-                        alert('Ты угадал!!! Поздравляем!!!');
-                        return;
-
-                    }
-
-                }
-
-            } else {
-
-                alert('Введи число!');
-
-            }
-            enterNumber();
+  },
+  getExpensesMonth: function () {
+    for (let key in appData.expenses) {
+      appData.expensesMonth += appData.expenses[key];
     }
-    enterNumber();
+    console.log("Расходы на месяц: " + appData.expensesMonth + " рублей");
+  },
+  getAccumulatedMonth: function () {},
+  getTargetMonth: function () {
+    appData.period = appData.period;
+    console.log(
+      "Цель будет достигнута за: " + Math.ceil(appData.period) + " месяцев"
+    );
+  },
 
+  getBudget: function () {
+    appData.budgetMonth = money - appData.expensesMonth;
+
+    appData.budgetDay = +Math.floor(appData.budgetMonth / 30);
+  },
+
+  getStatusIncome: function () {
+    if (appData.budgetDay >= 1200) {
+      console.log("У вас высокий уровень дохода");
+    } else if (appData.budgetDay >= 600) {
+      console.log("У вас средний уровень дохода");
+    } else if (appData.budgetDay < 600) {
+      console.log("К сожалению у вас уровень дохода ниже среднего");
+    } else if (appData.budgetDay <= 0) {
+      console.log("Что-то пошло не так");
+    }
+  },
+};
+appData.asking();
+
+appData.getExpensesMonth();
+
+appData.getBudget();
+
+appData.getTargetMonth();
+
+appData.getStatusIncome();
+
+for (let key in appData) {
+  console.log(
+    "Наша программа включает в себя данные:  " + key + ": " + appData[key]
+  );
 }
-askNumber();
