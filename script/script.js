78cffed48f2203baedbdd0d34f5ebfffe3d1092d
lelikone777 <lelikone777@users.noreply.github.com>
2020-07-02 21:33:31 +0300

    1

---
+++ script/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let startButton = document.getElementById('start');
+let buttonPlus1 = document.getElementsByTagName('button')[0];
+let buttonPlus2 = document.getElementsByTagName('button')[1];
+let depositCheck = document.querySelector('#deposit-check');
+let additionalIncomeItems = document.querySelectorAll('.additional_income-item');
+let values = document.querySelectorAll('.result-total');
+console.log(values);
