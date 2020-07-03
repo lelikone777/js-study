cfc7ff2f3ba6c837f5f872d68ab35da71b93a66c
lelikone777 <2257855@users.noreply.github.com>
2020-07-03 14:01:51 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
 let startButton = document.getElementById('start');
+console.log(startButton);
 let buttonPlus1 = document.getElementsByTagName('button')[0];
 let buttonPlus2 = document.getElementsByTagName('button')[1];
 let depositCheck = document.querySelector('#deposit-check');
 let additionalIncomeItems = document.querySelectorAll('.additional_income-item');
-let values = document.querySelectorAll('.result-total');
-console.log(values);
+let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+console.log(budgetDayValue);
+let budgetMonthValue = document.getElementsByClassName('buget_month-value')[0];
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+let getAccumulatedMonthValue = document.getElementsByClassName('accumulated_month-value')[0];
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+console.log(additionalIncomeValue);
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+let targetMonthValue = document.getElementsByClassName('target_month-value')[0];
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let expensesTitle = document.querySelector('.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpenses = document.querySelector('.additional_expenses');
+let periodSelect = document.querySelector('.period-select');
