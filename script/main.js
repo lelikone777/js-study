a5a36a465816ece3641c8cd31a15abfc2e0b9980
KALININ ALEXEY <2257855@gmail.com>
2021-02-07 17:28:19 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,12 +1,14 @@
 let money = 50000;
 let income = "freelance";
-let addExpenses = "интернет, такси, коммуналка";
+let addExpenses = "Интернет, ТаксИ, КоммуналкА";
 let deposit = true;
 let mission = 1000000;
 let period = 12;
+let budgetDay = money / 30;
 
 console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
-console.log(addExpenses.toLowerCase);
+console.log(addExpenses.toLowerCase().split(", "));
+console.log(budgetDay);
