dabedc83b204712b991d047392a10088e166230d
KALININ ALEXEY <2257855@gmail.com>
2021-02-21 22:14:25 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,16 +1,28 @@
-'use strict';
+"use strict";
 
-let money = 50000;
+let money = +prompt("Ваш месячный доход?");
 let income = "freelance";
-let addExpenses = "Интернет, ТаксИ, КоммуналкА";
-let deposit = true;
+let addExpenses = prompt(
+  "“Перечислите возможные расходы за рассчитываемый период через запятую”"
+);
+let deposit = confirm("“Есть ли у вас депозит в банке?”");
+
+let expenses1 = prompt("“Введите обязательную статью расходов?”");
+let amount1 = +prompt("“Во сколько это обойдется?”");
+
+let expenses2 = prompt("“Введите обязательную статью расходов?”");
+let amount2 = +prompt("“Во сколько это обойдется?”");
+
 let mission = 1000000;
 let period = 12;
-let budgetDay = money / 30;
 
-console.log(typeof money, typeof income, typeof deposit);
-console.log(addExpenses.length);
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
-console.log(addExpenses.toLowerCase().split(", "));
-console.log(budgetDay);
+let budgetDay = money / 30;
+let budgetMonth = money - (amount1 + amount2);
+console.log(budgetMonth);
+
+// console.log(typeof money, typeof income, typeof deposit);
+// console.log(addExpenses.length);
+// console.log("Период равен " + period + " месяцев");
+// console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
+// console.log(addExpenses.toLowerCase().split(", "));
+// console.log(budgetDay);
