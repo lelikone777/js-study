fee32cf5054aa6193c86f428a2ed6ac20d665744
KALININ ALEXEY <2257855@gmail.com>
2021-02-22 00:07:25 +0300

    1

--- script/main.js
+++ script/main.js
@@ -1,24 +1,25 @@
 "use strict";
 
 let money = +prompt("Ваш месячный доход?");
-let income = "freelance";
-let addExpenses = prompt(
-  "“Перечислите возможные расходы за рассчитываемый период через запятую”"
-);
-let deposit = confirm("“Есть ли у вас депозит в банке?”");
+// let income = "freelance";
+// let addExpenses = prompt(
+//   "“Перечислите возможные расходы за рассчитываемый период через запятую”");
+// let deposit = confirm("“Есть ли у вас депозит в банке?”");
 
-let expenses1 = prompt("“Введите обязательную статью расходов?”");
+// let expenses1 = prompt("“Введите обязательную статью расходов?”");
 let amount1 = +prompt("“Во сколько это обойдется?”");
 
-let expenses2 = prompt("“Введите обязательную статью расходов?”");
+// let expenses2 = prompt("“Введите обязательную статью расходов?”");
 let amount2 = +prompt("“Во сколько это обойдется?”");
 
-let mission = 1000000;
-let period = 12;
-
-let budgetDay = money / 30;
 let budgetMonth = money - (amount1 + amount2);
-console.log(budgetMonth);
+console.log("Бюджет на месяц:" + budgetMonth);
+let budgetDay = Math.floor(budgetMonth / 30);
+console.log("Бюджет на день:" + budgetDay);
+
+let mission = 1000;
+let period = Math.ceil(mission / budgetMonth);
+console.log("Цель будет достигнута через " + period + " месяцев");
 
 // console.log(typeof money, typeof income, typeof deposit);
 // console.log(addExpenses.length);
@@ -26,3 +27,13 @@ console.log(budgetMonth);
 // console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
 // console.log(addExpenses.toLowerCase().split(", "));
 // console.log(budgetDay);
+
+  if (budgetDay >= 1200) {
+    console.log("У вас высокий уровень дохода");
+  } else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log("У вас средний  уровень дохода");
+  } else if (budgetDay < 600 && budgetDay >= 0) {
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+  } else {
+    console.log("“Что то пошло не так”");
+  }
